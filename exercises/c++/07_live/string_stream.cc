eb8916fb20ff1080834f1b09121b6ee4bfa7de9a
ThorbenFroehlking <thorben.froehlking@web.de>
2018-12-11 08:20:01 +0100

    update

--- exercises/c++/07_live/string_stream.cc
+++ exercises/c++/07_live/string_stream.cc
@@ -2,13 +2,13 @@
 #include <sstream>
 
 int main() {
-  std::string base_name{"file_"};
-  for (auto i = 0; i < 10; ++i) {
-    std::stringstream ss;
-    ss << base_name << i << ".txt";
-    std::ofstream f{ss.str()};
-    f << i << std::endl;
-  }
+        std::string base_name{"file_"};
+        for (auto i = 0; i < 10; ++i) {
+                std::stringstream ss;
+                ss << base_name << i << ".txt";
+                std::ofstream f{ss.str()};
+                f << i << std::endl;
+        }
 
-  return 0;
+        return 0;
 }
